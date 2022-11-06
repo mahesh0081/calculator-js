77e0357fee6b54c30ca885f76925e1713dc49f62
Mahesh <maheshsona28@gmail.com>
2022-11-06 15:01:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    $('#zero').click(function () {
+        document.forms.display.value += 0;
+    });
+    $('#one').click(function () {
+        document.forms.display.value += 1;
+    });
+    $('#two').click(function () {
+        document.forms.display.value += 2;
+    });
+    $('#three').click(function () {
+        document.forms.display.value += 3;
+    });
+    $('#four').click(function () {
+        document.forms.display.value += 4;
+    });
+    $('#five').click(function () {
+        document.forms.display.value += 5;
+    });
+    $('#six').click(function () {
+        document.forms.display.value += 6;
+    });
+    $('#seven').click(function () {
+        document.forms.display.value += 7;
+    });
+    $('#eight').click(function () {
+        document.forms.display.value += 8;
+    });
+    $('#nine').click(function () {
+        document.forms.display.value += 9;
+    });
+    $('#add').click(function () {
+        document.forms.display.value += '+';
+    });
+    $('#subs').click(function () {
+        document.forms.display.value += '-';
+    });
+    $('#multi').click(function () {
+        document.forms.display.value += '*';
+    });
+    $('#divide').click(function () {
+        document.forms.display.value += '/';
+    });
+    $('#dot').click(function () {
+        document.forms.display.value += '.';
+    });
+    $('#equal').click(function () {
+        if(display.value == "") {
+            alert("Please enter any num to calculator")
+        }
+        else{
+            document.forms.display.value =
+            eval(document.forms.display.value);
+        }
+    });
+    $('#clear').click(function () {
+        document.forms.display.value = "";
+    });
+})
